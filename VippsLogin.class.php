74332a20c8a20569eecf15ea0417e07f6b985b88
Iver Odin Kvello <iver@wp-hosting.no>
2019-09-13 19:50:28 +0200

    frameworks

--- VippsLogin.class.php
+++ VippsLogin.class.php
@@ -1,10 +1,12 @@
 <?php 
+// VippsLogin: This singleton object does basically all the work 
 
 class VippsLogin {
   public $options = array();
   public $settings = array();
  
   function __construct() {
+     $this->settings = get_option('vipps_login_options', array());
   }
 
   // We are going to do the Singleton pattern here so 
@@ -52,22 +54,104 @@ class VippsLogin {
 
   }
 
-  public function login_form_continue_with_vipps () {
-     echo "<div style='margin:20px;' class='continue-with-vipps'><input style='width:100%' type=button value='Login with Vipps yo!'/></div>";
+  // Get the app token, store it for about half the time it will be valid for (just to have a margin)
+  private function vipps_app_token () {
+   $content = get_transient('_vipps_login_app_token');;
+   if ($content) return $content['access_token'];
+
+   $res = $this->get_auth_token();
+   if (empty($res) || $res['response'] != 200) {
+     return null;
+   }
+   $content = $res['content'];
+   $content['when'] = time();
+
+   set_transient('_vipps_login_app_token',$content,($content['expires_in']/2));
+
+   $tok = $content['access_token'];
+   return $tok;
   }
 
-  public function continue_from_vipps () {
+  // For a given redirect (operation, actually) and code (nonce, as returned from the Auth call), get an auth token
+  private function get_auth_token($code,$forwhat) {
+      $clientid= $this->settings['clientid'];
+      $secret = $this->settings['clientsecret'];
+      $redir  = $this->make_callback_url($forwhat);
+
+      # IOK FIXME better use .wellknown for this
+      $url = $this->backendUrl("oauth2/token");
+
+      $headers = array();
+      $headers['Authorization'] = "Basic " . base64_encode("$clientid:$secret");
+
+      $args = array('grant_type'=>'authorization_code', 'code'=>$code, 'redirect_uri'=>$redirect);
+
+      return $this->http_call($url,$args,'POST',$headers,'url');
+  }
+
+  private function get_openid_userinfo($accesstoken) {
+      $headers = array();
+      $headers['Authorization'] = "Bearer $accesstoken";
+      # IOK FIXME better use .wellknown for this
+      $url = $this->backendUrl("oauth2/userinfo");
+      $args = array();
+
+      return $this->http_call($url,$args,'GET',$headers,'url');
+  }
+
+  public function getAuthRedirect($forwhat,$state=0,$scope="openid address birthDate email name phoneNumber") {
+    $url      = $this->backendUrl('oauth2/auth');
+    $redir    = $this->make_callback_url($forwhat);
+    $clientid = $this->settings['clientid'];
+    if (is_array($scope)) $scope = join(' ',$scope);
+
+    if (!$clientid) return "";
+    $args = array('client_id'=>$clientid, 'response_type'=>'code', 'scope'=>$scope, 'state'=>$state, 'redirect_uri'=>$redir);
+    return $url . '?' . http_build_query($args);
+  }
+
+  // Return the  URL to the oAuth-backend.
+  public function backendUrl ($command) {
+    return "https://apitest.vipps.no/access-management-1.0/access/" . $command;
+//    return "https://api.vipps.no/access-management-1.0/access/" . $command;
+  }
+
+  public function login_form_continue_with_vipps () {
+     $url = $this->getAuthRedirect('login',0);
+     if (!$url) return;
+     echo "<div style='margin:20px;' class='continue-with-vipps'><a href='$url' class='button' style='width:100%'>Login with Vipps yo!</a></div>";
+     return true;
+  }
+
+  // This is the actual handler for all returns from Vipps; it's extendable by using standard Wordpress actions/filters
+  public function continue_from_vipps ($forwhat) {
         wc_nocache_headers();
         // Actually, we're totally going to redirect somewhere here. FIXME
         status_header(200,'OK');
-        wp_die('Hooray you got here!');
+
+// FIXME
+// Errorhandling: 'error', 'error_description', 'state'
+
+        $code = $_REQUEST['code'];
+        $state = $_REQUEST['state'];
+        
+        $authtoken = $this->get_auth_token($code, $forwhat);
+        // Errorhandling! FIXME
+        $userinfo = $this-> get_openid_userinfo($authtoken);
+        // Errorhandling! FIXME
+
+        // Or preferrably, *filter* this so we can return 'handled' or not handled.
+        // So this is what actually will do the several actions.
+        do_action('continue_with_vipps_' .  $forwhat, $state, $userinfo);
+
+        wp_die('Hooray you got here!' . print_r($_REQUEST,true));
   }
 
   public function make_callback_url ($forwhat) {
       if ( !get_option('permalink_structure')) {
-            return set_url_scheme(home_url(),'https') . "/?wp-vipps-login=$forwhatk&callback=";
+            return set_url_scheme(home_url(),'https') . "/?wp-vipps-login=continue-from-vipps&forwhat=$forwhat&callback=";
           } else {
-            return set_url_scheme(home_url(),'https') . "/wp-vipps-login/$forwhat?&callback=";
+            return set_url_scheme(home_url(),'https') . "/wp-vipps-login/continue-from-vipps/$forwhat";
          }
   }
 
@@ -80,11 +164,13 @@ class VippsLogin {
                 // IOK 2018-06-07 Change to add any prefix from home-url for better matching IOK 2018-06-07
                 if (preg_match("!/wp-vipps-login/$special/([^/]*)!", $_SERVER['REQUEST_URI'], $matches)) {
                     $method = $specialmethod; break;
+                    $forwhat = $matches[1];
                 }
             }
         } else {
             if (isset($_GET['wp-vipps-login'])) {
                 $method = @$specials[$_GET['wp-vipps-login']];
+                $forwhat = @$_GET['forwhat'];
             }
         }
         return $method;
@@ -244,7 +330,7 @@ class VippsLogin {
 
   public function activate () {
           // Options
-          $default = array('configpath'=>'');
+          $default = array('clientid'=>'','clientsecret'=>'');
 	  add_option('vipps_login_options',$default,false);
   }
 
@@ -261,9 +347,6 @@ class VippsLogin {
    $valid = array();
    foreach($input as $k=>$v) {
      switch ($k) {
-      case 'connect': 
-       $this->connecting=true;
-       break;
       default: 
        $valid[$k] = $v;
      }
@@ -277,29 +360,29 @@ class VippsLogin {
   }
   $options = get_option('vipps_login_options'); 
 
-  $clientid = $options['clientid'];
-
-  $connected = false;
-  if (!$connected) {
-    add_action('admin_notices',function () {
-       echo "<div class='notice notice-info is-dismissible'><p>Not connected to Beat backend - users cannot log in</p></div>";
-     });
-  }
 
 ?>
 <div class='wrap'>
  <h2><?php _e('Login With Vipps', 'vipps-login'); ?></h2>
+
 
 <?php do_action('admin_notices'); ?>
 
 <form action='options.php' method='post'>
 <?php settings_fields('vipps_login_options'); ?>
  <table class="form-table" style="width:100%">
+
    <tr>
        <td><?php _e('Client ID', 'login-vipps'); ?></td>
        <td width=30%><input id=configpath style="width:20em" name="vipps_login_options[clientid]" value="<?php echo htmlspecialchars($options['clientid']);?>" type="text"></td>
        <td><?php _e('Your client ID, from the Vipps Portal','vipps-login'); ?></td>
    </tr>
+   <tr>
+       <td><?php _e('Client Secret', 'login-vipps'); ?></td>
+       <td width=30%><input id=configpath style="width:20em" name="vipps_login_options[clientsecret]" value="<?php echo htmlspecialchars($options['clientsecret']);?>" type="xpassword"></td>
+       <td><?php _e('Your client secret, from the Vipps Portal','vipps-login'); ?></td>
+   </tr>
+
  </table>
   <div>
    <input type="submit" style="float:left" class="button-primary" value="<?php _e('Save Changes') ?>" />
@@ -307,11 +390,6 @@ class VippsLogin {
  </div>
 </form>
 
-<div class="wrap">
- <form name="connectform" method="post">
-   <input <?php if (!$keysok) print "disabled" ?> type="submit" style="float:right" name="connect" class="button-primary" value="<?php _e('Connect!','login-vipps') ?>" />
- </form>
-</div>
 
 </div>
 
