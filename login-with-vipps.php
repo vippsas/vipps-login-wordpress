c462d0bc0a2203e6b3691a501d4818f9e99ea167
Iver Odin Kvello <iver@wp-hosting.no>
2019-10-14 14:41:21 +0200

    Commentary

--- login-with-vipps.php
+++ login-with-vipps.php
@@ -2,10 +2,10 @@
 /*
    Plugin Name: Login with Vipps
    Version: 0.01
-   Description: Passwordless login and stuff using Vipps
+   Description: Passwordless login and more using Vipps
    Author: WP-Hosting AS
    Plugin URI: https://wordpress.org/plugins/login-with-vipps/
-   Description: Use Vipps for passwordless login and more
+   Description: Use Vipps for passwordless login and more. Integrates perfectly with WooCommerce.
    Author: WP Hosting
    Author URI: https://www.wp-hosting.no/
    Text-domain: login-with-vipps
@@ -20,9 +20,11 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
+// Utility classes used to verify JWT tokens and manage login sessions. IOK 2019-10-14
 require_once(dirname(__FILE__) . '/VippsSession.class.php');
 require_once(dirname(__FILE__) . '/VippsJWTVerifier.class.php');
 
+// This is the main class, a singleton. We'll store it in a global variable for hook-writing-convenience. IOK 2019-10-14
 global $ContinueWithVipps;
 require_once(dirname(__FILE__) . '/ContinueWithVipps.class.php');
 $ContinueWithVipps = ContinueWithVipps::instance();
@@ -39,6 +41,7 @@ if (is_admin()) {
     add_action('template_redirect',array($ContinueWithVipps,'template_redirect'));
 }
 
+// This class implements all login logic. IOK 2019-10-14
 global $VippsLogin;
 register_activation_hook(__FILE__,array($VippsLogin,'activate'));
 register_deactivation_hook(__FILE__,array($VippsLogin,'deactivate'));
@@ -54,6 +57,7 @@ if (is_admin()) {
     add_action('template_redirect',array($VippsLogin,'template_redirect'));
 }
 
+// And if WooCommerce is installed, integrate with that with another class. IOK 2019-10-14
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 if(is_plugin_active( 'woocommerce/woocommerce.php')) { 
     require_once(dirname(__FILE__) . '/WooLogin.class.php');
