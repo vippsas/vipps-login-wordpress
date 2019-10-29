45294ea79512d9531cc8478723737ad4c01395e7
Iver Odin Kvello <iver@wp-hosting.no>
2019-10-29 17:05:35 +0100

    Bump

--- login-with-vipps.php
+++ login-with-vipps.php
@@ -1,7 +1,7 @@
 <?php 
 /*
    Plugin Name: Login with Vipps
-   Version: 0.01
+   Version: 0.02
    Description: No need to remember passwords ever again. Vipps, and you are logged in.
    Author: WP-Hosting AS
    Plugin URI: https://wordpress.org/plugins/login-with-vipps/
