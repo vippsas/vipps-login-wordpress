33dedf932a32fbb610edd76b0c6008513ec74ffb
Iver Odin Kvello <iver@wp-hosting.no>
2020-08-03 10:43:05 +0200

    version

--- login-with-vipps.php
+++ login-with-vipps.php
@@ -9,7 +9,7 @@
    Author: WP Hosting
    Author URI: https://www.wp-hosting.no/
    Requires at least: 4.9.6
-   Tested up to: 5.4.2
+   Tested up to: 5.5.0
    Requires PHP: 7.0
    Text-domain: login-with-vipps
    Domain Path: /languages
