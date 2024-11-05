2f78c5af902a7b4e215b078bedbe982c17ee15ec
Lasse Pladsen <lasse.pladsen@hotmail.com>
2024-11-05 15:24:09 +0100

    Reformatting and cleanup

---
+++ blocks/login-with-vipps-button/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1cc10d215374d3c879a7');
--- src/blocks/webpack.config.js
+++ src/blocks/webpack.config.js
@@ -3,8 +3,11 @@ const path = require('path');
 
 module.exports = {
   ...defaultConfig,
+  entry: {
+    'login-with-vipps-button/index': './login-with-vipps-button/index.js',
+  },
   output: {
-    path: path.resolve(__dirname, '../blocks'),
+    path: path.resolve(__dirname, '../../blocks'),
     filename: '[name].js'
   }
 };
