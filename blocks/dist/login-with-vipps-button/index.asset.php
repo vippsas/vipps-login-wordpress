e25d28a9e8c5c3af2bbf15ef1d2b285dc03e609f
Iver Odin Kvello <iver@wp-hosting.no>
2026-02-26 09:59:50 +0000

    Fix css for new style wc blocks

--- blocks/dist/login-with-vipps-button/index.asset.php
+++ blocks/dist/login-with-vipps-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '532427a90621ecd5c74b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0c69c5081f8a049b3ad5');
