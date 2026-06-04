66e49ba7b2a3cfc0b3b05e825f8062b3f35594b9
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-06-04 08:34:05 +0200

    fix: login version constant

--- login-with-vipps.php
+++ login-with-vipps.php
@@ -50,7 +50,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-define('VIPPS_LOGIN_VERSION', '1.3.6');
+define('VIPPS_LOGIN_VERSION', '1.4.2');
 
 // Utility classes used to verify JWT tokens and manage login sessions. IOK 2019-10-14
 require_once(dirname(__FILE__) . '/VippsJWTVerifier.class.php');
