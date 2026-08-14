5c0386d898ad3c6e4834474f34009869936a779f
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-08-14 13:05:27 +0200

    feat: continue-with-vipps button in guten cart by using blockhooks

---
+++ blocks/dist/continue-with-vipps-cart/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'b377f280b4514449ca16');
---
+++ src/blocks/src/continue-with-vipps-cart/edit.tsx
@@ -0,0 +1,23 @@
+import { useBlockProps } from '@wordpress/block-editor';
+
+import { blockConfig } from './config';
+console.log( 'LP blockConfig: ', blockConfig );
+
+export default function Edit() {
+	return (
+		<div
+			{ ...useBlockProps( {
+				className:
+					'login-with-vipps-block continue-with-vipps-cart backend',
+			} ) }
+		>
+			{ /* The buy-now button. LP 2026-01-19 */ }
+			<a
+				title={ blockConfig[ 'title' ] }
+				dangerouslySetInnerHTML={ {
+					__html: blockConfig[ 'buttonHtml' ],
+				} }
+			></a>
+		</div>
+	);
+}
